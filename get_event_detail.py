1c877e17cd017496846f394a5325783c946a4041
Michael OConnor <gmikeoc@gmail.com>
2018-12-07 09:43:01 -0600

    Removed API gateway config info and referenced README.md file for detail

--- get_event_detail.py
+++ get_event_detail.py
@@ -14,21 +14,8 @@ from urllib.request import urlopen
     - A few attributes of the function execution context
 
     Since program returns CSS/HTML code vs. the typical json, important that
-    Integration Response in API Gateway is configured correctly. Specifically,
-
-    Integration Response
-
-    1. Navigate to the Integration Response for the API's GET method.
-    2. Open the 200 Response, Header Mappings, and Mapping Templates.
-    3. Edit the Response Header Content-Type and set the Mapping value
-        to 'text/html' (be sure to use single quotes). Save
-    4. Delete the application/json Content-Type under Mapping Templates.
-    5. Add the Content-Type: text/html (without quotes).
-    6. Select Mapping Template in the right-hand drop-down box.
-    7. Set the value of Template to:
-
-        #set($inputRoot = $input.path('$'))
-        $inputRoot
+    Integration Response in API Gateway is configured correctly. Please see
+    README.md for details steps on how to do this.
  
     Public domain by Michael OConnor <gmikeoc@gmail.com>
     Also available under the terms of MIT license
